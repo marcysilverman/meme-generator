9a9a8c43be2797c36bee5adb665213cb5f724a6f
Marcy Silverman <marcysilve225@gmail.com>
2019-02-20 10:05:47 -0800

    added more css

--- src/components/HeaderDisplay.js
+++ src/components/HeaderDisplay.js
@@ -7,10 +7,11 @@ function HeaderDisplay({ text, onChange, img, footer }) {
     <>
     <header className={styles.header}>
       <h1>Meme Generator </h1> 
+      <h2> Please upload an image first</h2>
       <div className={styles.display}>
-        <input type="text" name="text" value={text} onChange={onChange} placeholder="header"/>
-        <input type="text" name="footer" value={footer} onChange={onChange} placeholder="footer"/>
-        <input type="url" name="img" value={img} onChange={onChange} placeholder="image" />
+        <input type="text" name="text" value={text} onChange={onChange} placeholder="HEADER"/>
+        <input type="text" name="footer" value={footer} onChange={onChange} placeholder="FOOTER"/>
+        <input type="url" name="img" value={img} onChange={onChange} placeholder="IMAGE" />
       </div>
     </header>
     </>
